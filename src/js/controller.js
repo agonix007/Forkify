1129c8dae089a5be36b8ec6325c35af77816bfd8
Bikram <technology2203@gmail.com>
2025-03-17 00:54:25 +0530

    publisher - subscriber

--- src/js/controller.js
+++ src/js/controller.js
@@ -28,9 +28,7 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach(ev =>
-  window.addEventListener(ev, controlRecipes)
-);
-
-// window.addEventListener('hashchange', showRecipe);
-// window.addEventListener('load', showRecipe);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
