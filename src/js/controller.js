9d4eaaa381c3705015d02513505e2437f4652448
Bikram <technology2203@gmail.com>
2025-03-18 03:22:08 +0530

    storing bookmarks with localstorage

--- src/js/controller.js
+++ src/js/controller.js
@@ -22,14 +22,17 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
-    bookmarksView.update(model.state.bookmarks);
 
     // 1) Loading Recipe
     await model.loadRecipe(id);
 
     // 2) Rendering Recipe
     recipeView.render(model.state.recipe);
+
+    // 3) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
   } catch (err) {
+    console.log(err);
     recipeView.renderError();
   }
 };
@@ -86,7 +89,12 @@ const controlAddBookmark = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
