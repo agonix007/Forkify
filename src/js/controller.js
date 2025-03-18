d7be7399003378ecfd0ac7e0871023625e59a0b5
Bikram <technology2203@gmail.com>
2025-03-18 14:03:04 +0530

    uploading new recipe - part1

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,8 +4,9 @@ import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
-import 'core-js/stable';
+import 'core-js/actual';
 import 'regenerator-runtime/runtime';
 
 // if (module.hot) {
@@ -93,6 +94,12 @@ const controlBookmarks = function () {
   bookmarksView.render(model.state.bookmarks);
 };
 
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+
+  // Upload the new recipe data
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -100,5 +107,6 @@ const init = function () {
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView._addHandlerUpload(controlAddRecipe);
 };
 init();
