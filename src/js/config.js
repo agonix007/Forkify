d5d376bbb9ac023d1c4757bd5fe137595176ab1c
Bikram <technology2203@gmail.com>
2025-03-18 16:00:00 +0530

    uploading new recipe - part2

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '34bff8be-da6c-4dde-9f1c-28ea93fcb9a7';
+export const MODAL_CLOSE_SEC = 2.5;
